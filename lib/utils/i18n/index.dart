553b68e4154d170bd4f925185bd5f66396049bfe
shawn <dhryxy@gmail.com>
2021-02-22 17:35:35 +0800

    refactor i18n files for translation

--- lib/utils/i18n/index.dart
+++ lib/utils/i18n/index.dart
@@ -1,6 +1,9 @@
-import 'package:app/utils/i18n/account.dart';
-import 'package:app/utils/i18n/assets.dart';
-import 'package:app/utils/i18n/profile.dart';
+import 'package:app/utils/i18n/en/account.dart';
+import 'package:app/utils/i18n/en/assets.dart';
+import 'package:app/utils/i18n/en/profile.dart';
+import 'package:app/utils/i18n/zh/account.dart';
+import 'package:app/utils/i18n/zh/assets.dart';
+import 'package:app/utils/i18n/zh/profile.dart';
 
 const Map<String, Map<String, Map<String, String>>> i18n_full_dic_app = {
   'en': {
