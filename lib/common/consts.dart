e254a80ae1adbc216524530f26c28784ee3b89c2
shawn <dhryxy@gmail.com>
2021-05-17 19:12:09 +0800

    add keyboard auto hide & use cupertino route transition

--- lib/common/consts.dart
+++ lib/common/consts.dart
@@ -18,8 +18,8 @@ const local_tx_store_key = 'local_tx_store';
 
 /// app versions
 enum BuildTargets { apk, playStore, dev }
-const String app_beta_version = 'v2.0.6-beta.2';
-const int app_beta_version_code = 2062;
+const String app_beta_version = 'v2.0.6-beta.3';
+const int app_beta_version_code = 2063;
 
 const show_guide_status_key = 'show_guide_status';
 const show_banner_status_key = 'show_banner_status';
