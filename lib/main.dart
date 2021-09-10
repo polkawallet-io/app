e10801f8a74af6ea30184811359806317200a607
shawn <dhryxy@gmail.com>
2021-09-10 23:42:54 +0800

    remove JPush

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:polkawallet_plugin_acala/polkawallet_plugin_acala.dart';
+import 'package:polkawallet_plugin_bifrost/polkawallet_plugin_bifrost.dart';
 // import 'package:polkawallet_plugin_chainx/polkawallet_plugin_chainx.dart';
 import 'package:polkawallet_plugin_edgeware/polkawallet_plugin_edgeware.dart';
 import 'package:polkawallet_plugin_karura/polkawallet_plugin_karura.dart';
@@ -24,6 +25,7 @@ void main() async {
     PluginKarura(),
     PluginStatemine(),
     PluginAcala(),
+    PluginBifrost(),
     // PluginChainX(),
     PluginEdgeware(),
     PluginLaminar(),
