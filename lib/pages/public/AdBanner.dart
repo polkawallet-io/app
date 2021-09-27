125080917b1e08b79598a188fc7be0ce8ba5ce93
丁祖华 <dingzuhua@dingzuhuadeMacBook-Pro.local>
2021-09-27 11:19:11 +0800

    add banner_acala

--- lib/pages/public/AdBanner.dart
+++ lib/pages/public/AdBanner.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:app/common/consts.dart';
+import 'package:app/pages/profile/acalaCrowdLoan/acalaCrowdLoanPage.dart';
 import 'package:app/pages/profile/crowdLoan/crowdLoanBanner.dart';
 import 'package:app/pages/public/karCrowdLoanPage.dart';
 import 'package:app/pages/public/karCrowdLoanWaitPage.dart';
@@ -10,17 +11,18 @@ import 'package:app/utils/i18n/index.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:polkawallet_plugin_kusama/common/constants.dart';
 import 'package:polkawallet_sdk/api/types/networkParams.dart';
 import 'package:polkawallet_sdk/utils/i18n.dart';
 
 class AdBanner extends StatefulWidget {
-  AdBanner(this.service, this.connectedNode, this.changeToKusama,
+  AdBanner(this.service, this.connectedNode, this.switchNetwork,
       {this.canClose = false});
 
   final AppService service;
   final NetworkParams connectedNode;
   final bool canClose;
-  final Future<void> Function() changeToKusama;
+  final Future<void> Function(String) switchNetwork;
 
   @override
   _AdBannerState createState() => _AdBannerState();
@@ -44,26 +46,27 @@ class _AdBannerState extends State<AdBanner> {
   Future<void> _goToCrowdLoan(BuildContext context) async {
     final dic = I18n.of(context).getDic(i18n_full_dic_app, 'public');
 
-    if (_started['started'] || widget.service.buildTarget == BuildTargets.dev) {
-      if (widget.service.plugin.basic.name != 'kusama') {
-        showCupertinoDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return CupertinoAlertDialog(
-              title: Text(dic['auction.switch']),
-              content:
-                  Container(height: 64, child: CupertinoActivityIndicator()),
-            );
-          },
-        );
-        await widget.changeToKusama();
-        Navigator.of(context).pop();
-      }
-      Navigator.of(context)
-          .pushNamed(KarCrowdLoanPage.route, arguments: _started['endpoint']);
-    } else {
-      Navigator.of(context).pushNamed(KarCrowdLoanWaitPage.route);
+    // if (_started['started'] || widget.service.buildTarget == BuildTargets.dev) {
+    if (widget.service.plugin.basic.name != network_name_polkadot) {
+      showCupertinoDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return CupertinoAlertDialog(
+            title: Text(dic['auction.switch']),
+            content: Container(height: 64, child: CupertinoActivityIndicator()),
+          );
+        },
+      );
+      await widget.switchNetwork(network_name_polkadot);
+      Navigator.of(context).pop();
     }
+    //   Navigator.of(context)
+    //       .pushNamed(KarCrowdLoanPage.route, arguments: _started['endpoint']);
+    // } else {
+    //   Navigator.of(context).pushNamed(KarCrowdLoanWaitPage.route);
+    // }
+
+    Navigator.of(context).pushNamed(AcalaCrowdLoanPage.route);
   }
 
   @override
@@ -77,80 +80,94 @@ class _AdBannerState extends State<AdBanner> {
 
   @override
   Widget build(BuildContext context) {
-    final show = widget.connectedNode != null && _started['visible'];
+    // final show = widget.connectedNode != null && _started['visible'];
+    final show = true;
 
-    final fullWidth = MediaQuery.of(context).size.width;
-    final cardColor = Theme.of(context).cardColor;
+    // final fullWidth = MediaQuery.of(context).size.width;
+    // final cardColor = Theme.of(context).cardColor;
     return !show
         ? CrowdLoanBanner(widget.service, widget.connectedNode)
-        : Stack(
-            alignment: AlignmentDirectional.topEnd,
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
               GestureDetector(
-                child: Stack(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            margin: EdgeInsets.all(8),
-                            child: Image.asset(
-                                'assets/images/public/banner_kar_plo.png'),
-                          ),
-                        )
-                      ],
-                    ),
-                    Container(
-                      constraints: BoxConstraints(maxHeight: fullWidth / 4),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: fullWidth / 3 + 24,
-                            height: 32,
-                            margin:
-                                EdgeInsets.only(left: 16, top: 24, right: 8),
-                            // child: Image.asset(
-                            //     'assets/images/public/kar_logo.png'),
-                            child: SvgPicture.asset(
-                                'assets/images/public/kusama_logo.svg'),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 24, top: 8),
-                            child: Text(
-                              'Parachain Auction',
-                              style: TextStyle(
-                                  color: cardColor,
-                                  height: 0.9,
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  child:
+                      Image.asset("assets/images/public/banner_acala_pol.png"),
                 ),
                 onTap: () => _goToCrowdLoan(context),
-              ),
-              widget.canClose
-                  ? Container(
-                      padding: EdgeInsets.only(top: 12, right: 12),
-                      child: GestureDetector(
-                        child: Icon(
-                          Icons.cancel,
-                          color: Colors.white60,
-                          size: 16,
-                        ),
-                        onTap: () {
-                          widget.service.store.storage
-                              .write(show_banner_status_key, 'closed');
-                          widget.service.store.account.setBannerVisible(false);
-                        },
-                      ),
-                    )
-                  : Container()
+              )
             ],
           );
+    // : Stack(
+    //     alignment: AlignmentDirectional.topEnd,
+    //     children: [
+    //       GestureDetector(
+    //         child: Stack(
+    //           children: [
+    //             Row(
+    //               children: [
+    //                 Expanded(
+    //                   child: Container(
+    //                     margin: EdgeInsets.all(8),
+    //                     child: Image.asset(
+    //                         'assets/images/public/banner_kar_plo.png'),
+    //                   ),
+    //                 )
+    //               ],
+    //             ),
+    //             Container(
+    //               constraints: BoxConstraints(maxHeight: fullWidth / 4),
+    //               child: Column(
+    //                 crossAxisAlignment: CrossAxisAlignment.start,
+    //                 children: [
+    //                   Container(
+    //                     width: fullWidth / 3 + 24,
+    //                     height: 32,
+    //                     margin:
+    //                         EdgeInsets.only(left: 16, top: 24, right: 8),
+    //                     // child: Image.asset(
+    //                     //     'assets/images/public/kar_logo.png'),
+    //                     child: SvgPicture.asset(
+    //                         'assets/images/public/kusama_logo.svg'),
+    //                   ),
+    //                   Container(
+    //                     margin: EdgeInsets.only(left: 24, top: 8),
+    //                     child: Text(
+    //                       'Parachain Auction',
+    //                       style: TextStyle(
+    //                           color: cardColor,
+    //                           height: 0.9,
+    //                           fontSize: 22,
+    //                           fontWeight: FontWeight.bold),
+    //                     ),
+    //                   ),
+    //                 ],
+    //               ),
+    //             )
+    //           ],
+    //         ),
+    //         onTap: () => _goToCrowdLoan(context),
+    //       ),
+    //       Visibility(
+    //           visible: widget.canClose,
+    //           child: Container(
+    //             padding: EdgeInsets.only(top: 12, right: 12),
+    //             child: GestureDetector(
+    //               child: Icon(
+    //                 Icons.cancel,
+    //                 color: Colors.white60,
+    //                 size: 16,
+    //               ),
+    //               onTap: () {
+    //                 widget.service.store.storage
+    //                     .write(show_banner_status_key, 'closed');
+    //                 widget.service.store.account.setBannerVisible(false);
+    //               },
+    //             ),
+    //           ))
+    //     ],
+    //   );
   }
 }
