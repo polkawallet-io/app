57fba82d0db1157588171bf9c51b957b7889945b
dingzuhua <dingzuhua@qq.com>
2022-03-14 10:20:52 +0800

    delete bugly

--- lib/main-dev.dart
+++ lib/main-dev.dart
@@ -4,7 +4,7 @@ import 'package:app/common/types/pluginDisabled.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bugly/flutter_bugly.dart';
+// import 'package:flutter_bugly/flutter_bugly.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:polkawallet_plugin_acala/polkawallet_plugin_acala.dart';
 import 'package:polkawallet_plugin_bifrost/polkawallet_plugin_bifrost.dart';
@@ -15,37 +15,37 @@ import 'package:polkawallet_plugin_statemine/polkawallet_plugin_statemine.dart';
 import 'package:polkawallet_plugin_dbc/polkawallet_plugin_dbc.dart';
 
 void main() async {
-  FlutterBugly.postCatchedException(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
-    await GetStorage.init(get_storage_container);
-    await Firebase.initializeApp();
+  // FlutterBugly.postCatchedException(() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  await GetStorage.init(get_storage_container);
+  await Firebase.initializeApp();
 
-    final plugins = [
-      PluginKusama(name: 'polkadot'),
-      PluginKusama(),
-      PluginAcala(),
-      PluginKarura(),
-      PluginStatemine(),
-      PluginBifrost(),
-      // PluginChainX(),
-      PluginEdgeware(),
-      // PluginLaminar(),
-      PluginDBC(),
-    ];
+  final plugins = [
+    PluginKusama(name: 'polkadot'),
+    PluginKusama(),
+    PluginAcala(),
+    PluginKarura(),
+    PluginStatemine(),
+    PluginBifrost(),
+    // PluginChainX(),
+    PluginEdgeware(),
+    // PluginLaminar(),
+    PluginDBC(),
+  ];
 
-    runApp(WalletApp(
-        plugins,
-        [
-          PluginDisabled(
-              'chainx', Image.asset('assets/images/public/chainx_gray.png'))
-        ],
-        BuildTargets.dev));
-    FlutterBugly.init(
-      androidAppId: "64c2d01918",
-      iOSAppId: "3803dd717e",
-    );
-  });
+  runApp(WalletApp(
+      plugins,
+      [
+        PluginDisabled(
+            'chainx', Image.asset('assets/images/public/chainx_gray.png'))
+      ],
+      BuildTargets.dev));
+  //   FlutterBugly.init(
+  //     androidAppId: "64c2d01918",
+  //     iOSAppId: "3803dd717e",
+  //   );
+  // });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:app/common/consts.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bugly/flutter_bugly.dart';
+// import 'package:flutter_bugly/flutter_bugly.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:polkawallet_plugin_acala/polkawallet_plugin_acala.dart';
 import 'package:polkawallet_plugin_bifrost/polkawallet_plugin_bifrost.dart';
@@ -14,38 +14,38 @@ import 'package:polkawallet_plugin_karura/polkawallet_plugin_karura.dart';
 import 'package:polkawallet_plugin_kusama/polkawallet_plugin_kusama.dart';
 import 'package:polkawallet_plugin_statemine/polkawallet_plugin_statemine.dart';
 
-void main() {
-  FlutterBugly.postCatchedException(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
-    await GetStorage.init(get_storage_container);
-    await Firebase.initializeApp();
+void main() async {
+  // FlutterBugly.postCatchedException(() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  await GetStorage.init(get_storage_container);
+  await Firebase.initializeApp();
 
-    final plugins = [
-      PluginKusama(name: 'polkadot'),
-      PluginKusama(),
-      PluginAcala(),
-      PluginKarura(),
-      PluginStatemine(),
-      PluginBifrost(),
-      PluginChainX(),
-      PluginEdgeware(),
-      // PluginLaminar(),
-      PluginDBC(),
-    ];
+  final plugins = [
+    PluginKusama(name: 'polkadot'),
+    PluginKusama(),
+    PluginAcala(),
+    PluginKarura(),
+    PluginStatemine(),
+    PluginBifrost(),
+    PluginChainX(),
+    PluginEdgeware(),
+    // PluginLaminar(),
+    PluginDBC(),
+  ];
 
-    runApp(WalletApp(
-        plugins,
-        [
-          // PluginDisabled(
-          //     'chainx', Image.asset('assets/images/public/chainx_gray.png'))
-        ],
-        BuildTargets.apk));
-    FlutterBugly.init(
-      androidAppId: "64c2d01918",
-      iOSAppId: "3803dd717e",
-    );
-  });
+  runApp(WalletApp(
+      plugins,
+      [
+        // PluginDisabled(
+        //     'chainx', Image.asset('assets/images/public/chainx_gray.png'))
+      ],
+      BuildTargets.apk));
+  //   FlutterBugly.init(
+  //     androidAppId: "64c2d01918",
+  //     iOSAppId: "3803dd717e",
+  //   );
+  // });
 }
