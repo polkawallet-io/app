8ec10bccaf1e836b598c939faa579fc832921ac2
shawn <dhryxy@gmail.com>
2022-04-01 20:05:25 +0800

    transfer page ui update

--- lib/utils/i18n/zh/assets.dart
+++ lib/utils/i18n/zh/assets.dart
@@ -83,7 +83,7 @@ const Map<String, String> zhAssets = {
   'manage.hide': '隐藏 0 资产',
   'note': '提示',
   'note.msg1': '关闭帐户保护后，账户很可能会因为余额过低被网络移除，是否确认关闭',
-  'note.msg2': '非常抱歉，该账户中存在其他锁定资产，无法被移除。',
+  'note.msg2': '该账户中存在其他锁定资产，无法被移除。',
   'democracy.unlock': '可解锁',
   'scan': '扫一扫',
   'QRCode': '我的二维码',
