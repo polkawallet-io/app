0ecba526c6458cc0f68639920425ba3bd04d4b72
Shawn Yang <dhryxy@gmail.com>
2022-06-15 16:44:32 +0800

    New translations public.dart (Dutch)

--- lib/utils/i18n/nl/public.dart
+++ lib/utils/i18n/nl/public.dart
@@ -65,7 +65,6 @@ const Map<String, String> enPublic = {
   'hub.staking': 'Staking',
   'hub.cover.tokenStaking':
       'Provide your assets allocated on different networks in the ecosystem. You can send your assets through cross-chain transfer, or endow the liquidity to your assets through HOMA, so as to participate in more ecological projects.',
-  'ecosystem.on': 'On',
   'ecosystem.convertTo': 'Convert to',
   'ecosystem.crosschainTransfer': 'Crosschain Transfer',
   'ecosystem.from': 'From',
