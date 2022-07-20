40043daf3767e1dfa248b97ff851c22d169b61ed
dingzuhua <dingzuhua@qq.com>
2022-07-20 11:30:39 +0800

    guidePage ui update

--- lib/pages/public/guidePage.dart
+++ lib/pages/public/guidePage.dart
@@ -14,7 +14,7 @@ class GuidePage extends StatefulWidget {
 
 class _GuidePageState extends State<GuidePage> {
   final PageController _pageController = PageController();
-  final _pages = [0, 1, 2];
+  final _pages = [0, 1, 2, 3];
 
   int _pageIndex = 0;
 
@@ -25,7 +25,7 @@ class _GuidePageState extends State<GuidePage> {
     return WillPopScope(
         onWillPop: () async => false,
         child: Container(
-            color: Theme.of(context).scaffoldBackgroundColor,
+            color: const Color(0xFF242528),
             child: Stack(alignment: Alignment.bottomCenter, children: [
               PageView(
                 controller: _pageController,
@@ -35,54 +35,86 @@ class _GuidePageState extends State<GuidePage> {
                   });
                 },
                 children: _pages
-                    .map(
-                      (e) => Column(
-                        children: [
-                          Image.asset('assets/images/public/guide_$e.png')
-                        ],
-                      ),
-                    )
+                    .map((e) => Stack(
+                          alignment: Alignment.bottomLeft,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(top: 95),
+                              alignment: Alignment.topRight,
+                              child: Image.asset(
+                                'assets/images/public/guide_$e.png',
+                                width: double.infinity,
+                              ),
+                            ),
+                            Container(
+                              width: double.infinity,
+                              padding: EdgeInsets.only(
+                                  right: 120, left: 16, bottom: 182),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    dic['guide.title.$_pageIndex'],
+                                    textAlign: TextAlign.start,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline1
+                                        .copyWith(
+                                            fontSize:
+                                                UI.getTextSize(28, context),
+                                            color: Colors.white,
+                                            height: 1.15),
+                                  ),
+                                  Text(
+                                    dic['guide.text.$_pageIndex'],
+                                    textAlign: TextAlign.start,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline1
+                                        .copyWith(
+                                            fontSize:
+                                                UI.getTextSize(18, context),
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w400),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ))
                     .toList(),
               ),
               Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-                Container(
-                    width: double.infinity,
-                    margin: EdgeInsets.only(bottom: 44, left: 24, right: 24),
-                    child: Text(
-                      dic['guide.$_pageIndex'],
-                      textAlign: TextAlign.start,
-                      style: Theme.of(context).textTheme.headline1.copyWith(
-                          fontSize: UI.getTextSize(28, context), height: 1.15),
-                    )),
                 Padding(
-                    padding: EdgeInsets.only(left: 27),
+                    padding: EdgeInsets.only(left: 16),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: _pages
                           .map((i) => Container(
-                                margin: EdgeInsets.only(right: 19),
-                                height: 17,
-                                width: 17,
-                                // child: Text("$_pageIndex"),
+                                margin: EdgeInsets.only(right: 22),
+                                height: 18,
+                                width: 18,
                                 decoration: BoxDecoration(
                                     color: i == _pageIndex
-                                        ? _pageIndex == 0
-                                            ? Color(0xFFCE623C)
-                                            : _pageIndex == 1
-                                                ? Color(0xFFFFC952)
-                                                : Color(0xFF768FE1)
+                                        ? Color(0xFFFFC952)
                                         : Color(0xFFD5D2CD),
-                                    borderRadius: BorderRadius.all(
-                                        Radius.circular(19 / 2.0))),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(18))),
                               ))
                           .toList(),
                     )),
                 Padding(
-                    padding: EdgeInsets.fromLTRB(24, 16, 24, 30),
+                    padding: EdgeInsets.fromLTRB(16, 36, 16, 48),
                     child: Button(
+                      isDarkTheme: true,
                       title: _pageIndex + 1 >= _pages.length
                           ? dic['guide.enter']
                           : dic['guide.next'],
+                      style: Theme.of(context)
+                          .textTheme
+                          .button
+                          ?.copyWith(color: Colors.black),
                       onPressed: () {
                         if (_pageIndex + 1 >= _pages.length) {
                           data["storage"].write(data["storeKey"], true);
