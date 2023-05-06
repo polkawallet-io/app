bc68f3e4948a40e935eea00cbea06a2da33ab4e8
shawn <dhryxy@gmail.com>
2023-05-06 16:45:49 +0800

    announcementPage webview widget update

--- lib/pages/assets/announcementPage.dart
+++ lib/pages/assets/announcementPage.dart
@@ -1,9 +1,8 @@
 import 'package:app/utils/i18n/index.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:polkawallet_sdk/utils/i18n.dart';
 import 'package:polkawallet_ui/components/v3/back.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class AnnouncePageParams {
   AnnouncePageParams({this.title, this.link});
@@ -11,22 +10,35 @@ class AnnouncePageParams {
   final String link;
 }
 
-class AnnouncementPage extends StatelessWidget {
-  static final String route = '/announce';
+class AnnouncementPage extends StatefulWidget {
+  const AnnouncementPage({Key key}) : super(key: key);
+  static String route = '/announce';
+
+  @override
+  _AnnouncementPageState createState() => _AnnouncementPageState();
+}
+
+class _AnnouncementPageState extends State<AnnouncementPage> {
+  WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final AnnouncePageParams params = ModalRoute.of(context).settings.arguments;
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(params.link));
+  }
 
   @override
   Widget build(BuildContext context) {
     final Map dic = I18n.of(context).getDic(i18n_full_dic_app, 'assets');
-    final AnnouncePageParams params = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: AppBar(
           title: Text(dic['announce']), centerTitle: true, leading: BackBtn()),
       body: SafeArea(
-        child: WebView(
-          initialUrl: params.link,
-          javascriptMode: JavascriptMode.unrestricted,
-          gestureNavigationEnabled: true,
-        ),
+        child: WebViewWidget(controller: controller),
       ),
     );
   }
